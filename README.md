c886d0a89ee30c9e0ce3d6f2599adfa3a4f4050a
Razzwan <razvanlomov@gmail.com>
2020-09-16 17:35:45 +0300

    feat: first implementation

--- README.md
+++ README.md
@@ -1,63 +1,33 @@
-# react-use-recaptcha-v3
+# Redux API actions
 
-Integration [Google reCaptcha](https://developers.google.com/recaptcha/docs/v3) with [React hooks](https://reactjs.org/docs/hooks-intro.html).
+### Table of Contents
 
-# ![](https://www.gstatic.com/images/icons/material/product/2x/recaptcha_48dp.png) Google ReCaptcha V3 with React Hooks
+- [Getting Started](#getting-started)
+  - [Installation](#installation)
+  - [Usage](#usage)
+
+# Getting Started
 
 ## Installation
 
-`npm install react-use-recaptcha-v3 --save`
+```bash
+$ npm install --save redux-api-actions
+```
+
+or
+
+```bash
+$ yarn add redux-api-actions
+```
 
 ## Usage
 
-#### 1. Get your [site key for ReCaptcha V3](https://www.google.com/recaptcha/admin#v3signup)
-
-#### 2. Implement [Back End side](https://developers.google.com/recaptcha/docs/verify)
-
-#### 3. Use this package to send google re-captcha token to you Back End verifier
-
-##### simplest way:
 ```js
-import React from 'react'
-import { useReCaptcha } from 'react-use-recaptcha-v3';
+import { createAction } from 'redux-api-actions';
 
-const UncontrolledGoogleReCaptcha = () => {
-  const siteKey = '[SET UP YOU SITE KEY HERE!!!]';
-  const { token } = useReCaptcha(siteKey);
-
-  return (
-    <form>
-      {/* other fields here */}
-      {token ? <input name="g-recaptcha" value={token} /> : null}
-    </form>
-  );
-};
-
-```
-
-##### recommended way:
-```js
-import React, { useState } from 'react';
-import { useReCaptcha } from 'react-use-recaptcha-v3';
-
-const ControlledGoogleReCaptcha = () => {
-  const [token, setToken] = useState();
-
-  const handleToken = (tokenFromGoogleResponse) => {
-    console.log('fresh token is appear here, each time actionName is changed', { tokenFromGoogleResponse });
-    setToken(tokenFromGoogleResponse);
-  };
-
-  const siteKey = '[SET UP YOU SITE KEY HERE!!!]';
-  const actionName = 'submit'; // you can change actionName here if you need
-
-  useReCaptcha(siteKey, handleToken, actionName);
-
-  return (
-    <form>
-      {/* other fields here */}
-      {token ? <input name="g-recaptcha" value={token} /> : null}
-    </form>
-  );
-};
+const apiRequest = createAction('actionType', () => ({
+  request: {
+    url: '/users',
+  },
+}));
 ```
--- src/createApiAction.ts
+++ src/createApiAction.ts
@@ -1,80 +1,48 @@
-import { useCallback, useEffect, useState } from 'react'
+import {
+  Action,
+  ActionFunction0,
+  ActionFunction1,
+  ActionFunction2,
+  ActionFunction3,
+  ActionFunction4,
+  createAction,
+} from 'redux-actions';
 
-const SCRIPT_ID = 'g-recaptcha-script';
-
-const checkIsReady = () => window && (window as any).grecaptcha && typeof (window as any).grecaptcha.execute !== 'undefined';
-
-let interval: number | null = null;
-
-export const createApiAction = (siteKey: string, actionName: string = 'submit', verifyCallback?: (token: string) => any,  throwError: boolean = true): { execute: Promise<string>; isReady: boolean; } => {
-  const [isReady, setIsReady] = useState(false);
-
-  const testIsReady = useCallback(() => {
-    setIsReady(() => {
-      if (checkIsReady()) {
-        if (interval) {
-          clearInterval(interval);
-        }
-        return true
-      }
-
-      return false
-    })
-  }, [setIsReady]);
-
-  useEffect(() => {
-    if (typeof siteKey !== 'string') {
-      if (throwError) {
-        throw new Error('siteKey MUST be valid google reCaptcha siteKey. @see https://developers.google.com/recaptcha/docs/v3')
-      } else {
-        return;
-      }
-    }
-    // 1. try to find existing script
-    const existingScript = document.getElementById(SCRIPT_ID);
-    if (!existingScript) {
-      // 2. add the script to the dome if not exists
-      const script = document.createElement('script');
-      script.id = SCRIPT_ID;
-      script.type = 'text/javascript';
-      script.src = `https://www.google.com/recaptcha/api.js?render=${siteKey}`;
-      document.body.appendChild(script);
-    }
-    interval = setInterval(testIsReady, 1000) as any;
-    return () => {
-      if (interval) {
-        clearInterval(interval)
-      }
-      const el = document.getElementById(SCRIPT_ID);
-      if (el && el.remove) {
-        el.remove()
-      }
-    }
-  }, [siteKey, testIsReady]);
-
-  const execute = useCallback(async () => {
-    if (isReady) {
-      return (window as any).grecaptcha.execute(siteKey, { action: actionName });
-    } else {
-      throw new Error('You trying to execute grecaptcha.execute before script initialize');
-    }
-  }, [actionName, isReady, siteKey]);
-
-  useEffect(() => {
-    if (typeof actionName !== 'string') {
-      throw new Error('actionName MUST be a string [a-z_]')
-    }
-    if (isReady && verifyCallback) {
-      (async function() {
-        try {
-          const resToken = await execute();
-          verifyCallback(resToken);
-        } catch (e) {
-          console.error(e);
-        }
-      })();
-    }
-  }, [actionName, execute, isReady, verifyCallback])
-
-  return { execute, isReady };
+export interface IRequestPayload<D = any, P = any> {
+  data?: D;
+  method?: string;
+  params?: P;
+  url: string;
 }
+
+export interface IRequestAction<D = any, P = any> {
+  request: IRequestPayload<D, P>;
+}
+
+interface IAdditionalActions {
+  fail: string;
+  request: string;
+  success: string;
+}
+
+export type ActionType<P1 = any, P2 = any, P3 = any, P4 = any> = ActionFunction0<Action<IRequestPayload>> &
+  ActionFunction1<P1, Action<IRequestPayload>> &
+  ActionFunction2<P1, P2, Action<IRequestPayload>> &
+  ActionFunction3<P1, P2, P3, Action<IRequestPayload>> &
+  ActionFunction4<P1, P2, P3, P4, Action<IRequestPayload>>;
+
+export const createApiAction = <P1 = any, P2 = any, P3 = any, P4 = any>(
+  actionType: string,
+  payloadCreator: ActionFunction0<IRequestAction>
+    | ActionFunction1<P1, IRequestAction>
+    | ActionFunction2<P1, P2, IRequestAction>
+    | ActionFunction3<P1, P2, P3, IRequestAction>
+    | ActionFunction4<P1, P2, P3, P4, IRequestAction>
+): ActionType<P1, P2, P3, P4> & IAdditionalActions => {
+  const actionCreator = createAction<any, P1, P2, P3, P4>(actionType, payloadCreator) as any;
+  actionCreator.toString = () => actionType;
+  actionCreator.request = actionType;
+  actionCreator.success = actionType + '_SUCCESS';
+  actionCreator.fail = actionType + '_FAIL';
+  return actionCreator;
+};
